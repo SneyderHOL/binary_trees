766da3f15debbdbfea14884e4133921f90f24625
andres reyes <andressantiagore@gmail.com>
2020-07-17 23:58:49 +0000

    add two functions and fix it the indentation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,15 +15,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = value;
-	new_node->left = NULL;
 	new_node->right = NULL;
+	new_node->left = NULL;
 	new_node->parent = parent;
+
+
 	if (parent->left != NULL)
+	{
 		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+
 	parent->left = new_node;
 	return (new_node);
 }
