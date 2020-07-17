90e3e855e22f5d6b33b1de695c4dcb042a262f61
andres reyes <andressantiagore@gmail.com>
2020-07-17 22:43:19 +0000

    add function to tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,17 +2,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - function that creates a binary tree node
+ * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
  *
  * Return: pointer to the new node, or NULL on failure
- */
+*/
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -20,5 +22,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 	new_node->parent = parent;
+	if (parent->left != NULL)
+		new_node->left = parent->left;
+	parent->left = new_node;
 	return (new_node);
 }
