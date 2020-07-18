7f687ddd667017ca7f8bccb7f7f905debee73523
andres reyes <andressantiagore@gmail.com>
2020-07-18 03:32:55 +0000

    add the function files to manage binary tree modificated the second file

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: is the whole tree
+ * @func: is a pointer to a function to call for each node.
+ *
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL)
+		return;
+
+
+     func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
