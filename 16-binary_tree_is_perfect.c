3ed1aa02510af875465a1ec074b934d4ccbb4ab2
andres reyes <andressantiagore@gmail.com>
2020-07-23 21:55:26 +0000

    add the function to print in level with queue

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -79,10 +79,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_balance(tree) == 0)
-	{
 		if (binary_tree_is_full(tree) == 1)
 			return (1);
-	}
 
 	return (0);
 }
